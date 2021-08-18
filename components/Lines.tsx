edeb802f8ed7edf6cd88be708a3bd1e708e444b3
Jaza K <jaza-k@protonmail.com>
2021-08-17 22:25:24 -0600

    create separate lines component

---
+++ components/Lines.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+const Lines: React.FunctionComponent = () => (
+    <div className="lines">
+        <span className="line" id="top"></span>
+        <span className="line" id="bottom"></span>
+    </div>
+)
+
+export default Lines
